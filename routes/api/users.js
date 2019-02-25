d25f01da7b9c7665cc7623d8eb0e6699f540e02e
cwooz <woosley.christopher@gmail.com>
2019-02-25 18:30:37 -0500

    registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// Load User Model
+const user = require('../../models/User');
 
 
-// @route     GET api/users/test
-// @desc      Tests post route
-// @access    Public
+// Routes ------------------------------>
+
+// @Route     GET api/users/test
+// @Desc      Tests post route
+// @Access    Public
 router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
+
+// @Route     GET api/users/register
+// @Desc      Register user
+// @Access    Public
+router.post('/register', (req, res) => {
+	user.findOne({ email: req.body.email }).then(user => {
+		if (user) {
+			return res.status(400).json({ email: 'Email Already Exists' });
+		} else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm' // Default
+      });
+
+			const newUser = new User({
+				name: req.body.name,
+				email: req.body.email,
+				avatar: avatar,
+				password: req.body.password
+      });
+      
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if(err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        })
+      });
+		}
+	});
+});
+// ------------------------------>
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -16,6 +17,12 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+
+
+// Middleware ------------------------------->
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 // Routes ----------------------------------->
